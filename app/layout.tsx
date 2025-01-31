0048ca899ec65906bfdeb78a7946fda0cd37e31c
Gabi <gabinelepcu29@gmail.com>
2025-01-31 11:30:24 +0200

    portofolio update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Gabriel Nelepcu | Aspiring Python Developer & Future Freelancer",
   description:
       "Passionate beginner Python developer eager to learn and grow. Exploring the world of backend development, web applications, and freelancing opportunities.",
-  keywords: "Python, beginner developer, aspiring freelancer, learning, coding journey, backend development",
+  keywords: "Python, beginner developer, aspiring freelancer, learning, coding journey, backend development, gabriel nelepcu,nelepcu,gabriel",
 }
 
 export default function RootLayout({
