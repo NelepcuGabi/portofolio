e33ea025f0bd62e55b02dab62a00c260a9b6a0b0
Gabi <gabinelepcu29@gmail.com>
2025-07-22 13:11:03 +0300

    update corina

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,30 @@ import type React from "react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Gabriel Nelepcu | Aspiring Python Developer & Future Freelancer",
-  description:
-      "Passionate beginner Python developer eager to learn and grow. Exploring the world of backend development, web applications, and freelancing opportunities.",
-  keywords: "Python, beginner developer, aspiring freelancer, learning, coding journey, backend development, gabriel nelepcu,nelepcu,gabriel",
+    title: "Gabriel Nelepcu | Web & Python Developer – Portfolio & Projects",
+    description:
+        "Portfolio of Gabriel Nelepcu, Python developer focused on backend development, web apps, and freelancing. Check out my projects and coding journey.",
+    keywords:
+        "Python, web developer, backend developer, freelancer, Next.js, React, portfolio, Gabriel Nelepcu, AI projects, programming, software engineer",
+    authors: [{ name: "Gabriel Nelepcu", url: "https://gabrielnepcu.com" }],
+    creator: "Gabriel Nelepcu",
+    openGraph: {
+        title: "Gabriel Nelepcu – Python Developer Portfolio",
+        description:
+            "Check out my latest projects and coding journey in web & AI.",
+        url: "https://gabrielnepcu.ro",
+        siteName: "Gabriel Nelepcu Portfolio",
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Gabriel Nelepcu – Python Developer Portfolio",
+        description:
+            "Check out my latest projects and coding journey in web & AI.",
+        creator: "@numele_tău", // dacă ai cont de Twitter
+
+    },
 }
 
 export default function RootLayout({
